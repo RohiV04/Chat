250bf6024bcd40daa9e578be5b679c093ca9ea0a
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-06-02 12:45:36 +0530

    first commit

---
+++ screens/Signup.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  StatusBar,
+  Alert,
+} from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+const backImage = require("../assets/backImage.png");
+// const mailgun = require("mailgun.js");
+
+// const DOMAIN = "alumini.mydomain.com";
+// const mg = mailgun({
+//   apiKey: "71ef012ba27b550b2f2939198c155923-30344472-5ed21e6c",
+//   domain: DOMAIN,
+// });
+export default function Signup({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onHandleSignup = () => {
+    if (email !== "" && password !== "") {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => console.log("Signup success"))
+        // .then(() => sendmail(email))
+        .catch((err) => Alert.alert("Login error", err.message));
+    }
+  };
+  // function sendmail(email) {
+  //   const mailData = {
+  //     from: "21B91A6234@srkrec.ac.in",
+  //     to: email,
+  //     subject: "Confirm your Account ! ",
+  //     template: "account_confirm",
+  //   };
+  //   mg.messages().send(mailData, function (error, body) {
+  //     console.log(body);
+  //   });
+  // }
+  return (
+    <View style={styles.container}>
+      {/* <Image source={backImage} style={styles.backImage} /> */}
+      <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={onHandleSignup}>
+          <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
+            {" "}
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "600", fontSize: 14 }}>
+            Already have an account{" "}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text
+              style={{
+                color: "#E5BEEC",
+                fontWeight: "700",
+                fontStyle: "italic",
+                fontSize: 16,
+              }}
+            >
+              {" "}
+              Log In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2A2F4F",
+  },
+  title: {
+    fontSize: 48,
+    fontWeight: "bold",
+    color: "#E5BEEC",
+    alignSelf: "center",
+    paddingBottom: 37,
+  },
+  input: {
+    backgroundColor: "#FDE2F3",
+    height: 58,
+    marginBottom: 20,
+    fontSize: 16,
+    borderRadius: 10,
+    padding: 12,
+  },
+  backImage: {
+    width: "100%",
+    height: 340,
+    position: "absolute",
+    top: 0,
+    resizeMode: "cover",
+  },
+  whiteSheet: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    //bottom: 0,
+    backgroundColor: "#2A2F4F",
+    borderTopLeftRadius: 60,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 30,
+  },
+  button: {
+    backgroundColor: "#E5BEEC",
+    height: 58,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+});

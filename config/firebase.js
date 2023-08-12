862a5af43ab9df1e4c9323490b291236d03415f4
RohiV04 <rohith_csd@srkrec.edu.in>
2023-08-12 15:17:58 +0530

    Added Fire baseCredientials

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import Constants from 'expo-constants';
 // Firebase config
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId,
-  databaseURL: Constants.manifest.extra.databaseURL
+  apiKey: "AIzaSyCKio2r9gyCuCEDp5vXss9UHiHjw4Hxwfk",
+  authDomain: "chat-app-9abdf.firebaseapp.com",
+  projectId:"chat-app-9abdf",
+  storageBucket: "chat-app-9abdf.appspot.com",
+  messagingSenderId: "817216851574",
+  appId: "1:817216851574:web:7c81b822993b418d525957",
+  databaseURL: "G-9KP5LE7HG9"
 };
 // initialize firebase
 initializeApp(firebaseConfig);
